7b0cb46b2c42fa356a1f034b169193fa7f112207
adamzielinski972 <102333641+adamzielinski972@users.noreply.github.com>
2023-12-14 00:54:43 -0500

    Update update_workflows.py

--- update_workflows.py
+++ update_workflows.py
@@ -1,11 +1,10 @@
 from github import Github
 
 # GitHub credentials
-github_token = 'REPO_C_ACCESS_TOKEN'  # Replace with your GitHub token
-org_name = 'adamzielinski972'  # Replace with your organization name
+github_token = 'WORKFLOW_TOKEN'  # Replace with your GitHub token
 
 # List of repositories where you want to update workflows
-repos_to_update = ['testRepoA', 'testRepoB', 'testRepoC']  # Replace with repository names
+repos_to_update = ['repo_A', 'repo_B', 'repo_C']  # Replace with repository names
 
 # Workflow file content to be updated
 updated_workflow_content = """
@@ -26,10 +25,10 @@ jobs:
 def update_workflow(repo):
     try:
         # Get the workflow file
-        workflow_file = repo.get_contents('.github/workflows/blank.yml')
+        workflow_file = repo.get_contents('.github/workflows/main.yml')
         
         # Update the workflow file content
-        repo.update_file('.github/workflows/blank.yml', 'Updated workflow', updated_workflow_content, workflow_file.sha)
+        repo.update_file('.github/workflows/main.yml', 'Updated workflow', updated_workflow_content, workflow_file.sha)
         print(f"Workflow updated in {repo.full_name}")
     except Exception as e:
         print(f"An error occurred while updating workflow in {repo.full_name}: {str(e)}")
@@ -37,11 +36,10 @@ def update_workflow(repo):
 def main():
     # Authenticate to GitHub
     g = Github(github_token)
-    org = g.get_organization(org_name)
 
     # Update workflows in each repository
     for repo_name in repos_to_update:
-        repo = org.get_repo(repo_name)
+        repo = g.get_repo(f'{adamzielinski972}/{repo_name}')
         update_workflow(repo)
 
 if __name__ == "__main__":
