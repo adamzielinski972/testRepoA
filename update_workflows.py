6e082f89c7aa5165949c2fc2c5c7c7c364fd6391
adamzielinski972 <102333641+adamzielinski972@users.noreply.github.com>
2023-12-14 16:41:58 -0500

    Update update_workflows.py

--- update_workflows.py
+++ update_workflows.py
@@ -13,11 +13,13 @@ repos_to_update = ['testRepoA', 'testRepoB', 'testRepoC']  # Replace with reposi
 
 # Workflow file content to be updated
 updated_workflow_content = """
-name: Updated Workflow
+name: Test
 on:
   push:
     branches:
       - main
+#Something went wrong
+
 jobs:
   build:
     runs-on: ubuntu-latest
@@ -31,6 +33,21 @@ def update_workflow(repo):
     try:
         # Get the workflow file
         workflow_file = repo.get_contents('.github/workflows/blank.yml')
+        old_content = workflow_file.decoded_content.decode('utf-8')
+
+        # Preserve line 7 content
+        lines = old_content.split('\n')
+        line_7 = lines[6]
+
+        # Update the workflow file content excluding line 7
+        updated_content = "\n".join(lines[:6] + lines[7:])  # Exclude line 7
+
+        # Append the updated content to retain line 7 and other contents
+        updated_workflow_content = updated_content + f"\n      {line_7}\n" + """
+          - name: Updated Step
+            run: echo 'Updated step'
+          # Add more updated steps here
+        """
         
         # Update the workflow file content
         repo.update_file('.github/workflows/blank.yml', 'Updated workflow', updated_workflow_content, workflow_file.sha)
